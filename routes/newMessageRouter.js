c3e37ac3c3cdb67495409c5cd5b74b87c7709366
rushil <rushiljalal@gmail.com>
2024-12-19 14:28:24 +0530

    add moment-timezone for handling time zones in message timestamps

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const pool = require("../db/config");
 const newMessageRouter = Router()
+const moment = require("moment-timezone")
 
 //render form.ejs
 newMessageRouter.get("/", (req, res) => {
@@ -10,8 +11,10 @@ newMessageRouter.get("/", (req, res) => {
 //handle form post request and insert into db
 newMessageRouter.post("/", async (req, res) => {
     const { name, message } = req.body
+    const indiaTime = moment().tz("Asia/Kolkata").format();
+
     try {
-        await pool.query('insert into messages (text,"user",added) values ($1,$2,$3)', [message, name, new Date()])
+        await pool.query('insert into messages (text,"user",added) values ($1,$2,$3)', [message, name, indiaTime])
         res.redirect("/")
     } catch (error) {
         console.error(error)
