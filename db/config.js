4f14d1424732a0f36fd25be5bd007d0eea1e92e3
rushil <rushiljalal@gmail.com>
2025-01-02 19:10:09 +0530

    idk

--- db/config.js
+++ db/config.js
@@ -6,14 +6,16 @@ let retries = 0;
 
 const createPool = () => {
     return new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        // ssl: {
-        //     rejectUnauthorized: false
-        // }
+        // user: process.env.DB_USER,
+        // host: process.env.DB_HOST,
+        // database: process.env.DB_NAME,
+        // password: process.env.DB_PASSWORD,
+        // port: process.env.DB_PORT,
+
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     });
 };
 
